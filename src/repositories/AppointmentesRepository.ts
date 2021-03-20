05f12a9d96061fbc9af1259eae5d8f96e2f08d26
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2021-03-19 21:20:40 -0300

    Creating ModelAppointment and RotaAppointment separetion2

---
+++ src/repositories/AppointmentesRepository.ts
@@ -0,0 +1,11 @@
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create(provider: string, date: Date) {}
+}
